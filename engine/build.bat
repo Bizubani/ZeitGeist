f9f82ae7714e3024989c475557f4595f6f65508b
Bizubani <bizubani@gmail.com>
2021-04-24 17:30:37 -0400

    Added the engine folder and the windows build command

---
+++ engine/build.bat
@@ -0,0 +1,26 @@
+REM Build script for the engine. Will be replaced with cmake in the future
+@ECHO OFF   
+REM Force variables to wait till execution time to give their values
+SetLocal EnableDelayedExpansion
+
+REM Get a list of all the .c files
+SET cFilenames=
+FOR /R %%f in (*.c) do (
+    SET cFilenames=!cFilenames! %%f
+)
+
+REM echo "Files:" %cFilenames%
+
+SET assembly=engine
+SET compilerFlags=-g -shared -Wvarargs -Wall -Werror
+REM -Wall -Werror
+SET includeFlags=-Isrc -I%VULKAN_SDK%/Include
+SET linkerFlags=-luser32 -lvulkan-1 -L%VULKAN_SDK%/Lib
+SET defines=-D_DEBUG -DKEXPORT -D_CRT_SECURE_NO_WARNINGS
+
+ECHO "Building %assembly%%.."
+clang %cFilenames% %compilerFlags% -o ../bin/%assembly%.dll %defines% %includeFlags% %linkerFlags%
+
+
+
+
